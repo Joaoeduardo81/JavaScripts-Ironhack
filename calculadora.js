ad3f72b685596b7dc770579358d29790254d3fb5
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2018-10-13 16:45:34 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,37 @@
+
+//sum
+
+function myCalculatorSum (a, b) {
+  var soma = a + b;
+  return soma;
+};
+
+console.log(myCalculatorSum(2,2));
+
+//subtraction
+
+function myCalculatorMinus (a, b) {
+  var minus = a - b;
+  return minus;
+}
+
+console.log(myCalculatorMinus(2,2));
+
+// division
+
+function myCalculatorDivide (a, b) {
+  var divide = a / b;
+  return divide;
+}
+
+console.log(myCalculatorDivide(2,2));
+
+// multiply
+
+function myCalculatorMultiply (a, b) {
+  var multiply = a * b;
+  return multiply;
+}
+
+console.log(myCalculatorMultiply(2,2));
+
